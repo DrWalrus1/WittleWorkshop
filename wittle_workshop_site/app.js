5df72dcbab998a594f35a83a3e794f73d5a3cddb
Luke Lynch <Tech.Luk3@gmail.com>
2024-02-10 15:49:47 +1100

    Added basic HTML page and JavaScript. Also added code-workspace

---
+++ wittle_workshop_site/app.js
@@ -0,0 +1,19 @@
+async function CallContainerApi(command) {
+    let request = await fetch("http://localhost:8000/container",
+        {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
+            body: JSON.stringify({
+                command: "Start"
+            })
+        }).then(function (result) {
+            return result;
+        });
+
+        console.log(await request.json())
+}
