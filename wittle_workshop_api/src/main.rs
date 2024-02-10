b3bb5b5f374b25390f6a19c53a315adc8b0ad50b
Luke Lynch <Tech.Luk3@gmail.com>
2024-02-10 15:50:33 +1100

    Added CORS to API

--- wittle_workshop_api/src/main.rs
+++ wittle_workshop_api/src/main.rs
@@ -4,6 +4,37 @@ extern crate rocket;
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use std::fmt;
 use std::fmt::Formatter;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, Method, Status};
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if request.method() == Method::Options {
+            response.set_status(Status::NoContent);
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, PATCH, GET, DELETE",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        }
+        response.set_header(Header::new(
+            "Access-Control-Allow-Origin",
+            "*",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 enum ContainerCommand {
@@ -25,19 +56,20 @@ struct ContainerRequest {
     command: ContainerCommand,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ContainerResponse {
     result: String,
 }
 
 #[post("/container", data = "<container_request>")]
 fn hello(container_request: Json<ContainerRequest>) -> Json<ContainerResponse> {
-    return Json::from(ContainerResponse {
+    let result = ContainerResponse {
         result: container_request.command.to_string(),
-    });
+    };
+    return Json::from(result);
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().attach(CORS).mount("/", routes![hello])
 }
