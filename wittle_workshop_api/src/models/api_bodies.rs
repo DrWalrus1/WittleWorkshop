e9acc6fd81977fd2486c9cc54a8fd3aca405a720
Luke Lynch <luke@lukelynch.io>
2024-02-13 22:05:36 +1100

    removed unused imports

--- wittle_workshop_api/src/models/api_bodies.rs
+++ wittle_workshop_api/src/models/api_bodies.rs
@@ -1,6 +1,4 @@
-use rocket::{http::ContentType, response::{self, Responder}, Request, Response};
 use serde::{Deserialize, Serialize};
-
 use crate::commands::docker_commands::ContainerCommand;
 
 #[derive(Serialize, Deserialize)]
