535dae20925a503578c7cb3e2c85e6ab2b3874d4
Luke Lynch <Tech.Luk3@gmail.com>
2024-06-10 21:48:01 +1000

    Commenting out containers path. bug found

--- src/routes/docker_routes.rs
+++ src/routes/docker_routes.rs
@@ -1,5 +1,6 @@
 use crate::models::api_bodies::ApiResponse;
 use crate::Config;
+use dockworker::container::ContainerFilters;
 use dockworker::image::SummaryImage;
 use dockworker::Docker;
 use rocket::{get, post};
@@ -10,6 +11,7 @@ use serde::Serialize;
 pub async fn docker_base_get(path: &str, state: &rocket::State<Config>) -> ApiResponse<Vec<serde_json::Value>> {
     match path {
         "images" => ApiResponse::Ok(Json(convert_to_json(get_images(state).await))),
+        // "containers" => ApiResponse::Ok(Json(convert_to_json(get_containers(state).await))),
         "networks" => ApiResponse::Ok(Json(convert_to_json(get_networks(state).await))),
         _ => ApiResponse::NotFound("Invalid path".to_string()),
     }
@@ -37,6 +39,13 @@ pub async fn get_images(state: &rocket::State<Config>) -> Vec<SummaryImage> {
 }
 
 // ----- CONTAINERS -----
+pub async fn get_containers(state: &rocket::State<Config>) -> Vec<dockworker::container::Container> {
+    let docker = &state.docker;
+    let containers = docker.list_containers(None, None, None, ContainerFilters::default()).await.unwrap();
+
+    containers
+}
+// ----- Networks -----
 pub async fn get_networks(state: &rocket::State<Config>) -> Vec<dockworker::network::Network> {
     let docker = &state.docker;
     let networks = docker.list_networks(Default::default()).await.unwrap();
